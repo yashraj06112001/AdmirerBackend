fd47a5d0cae0e59fc78da800197dae46223c74f5
yash raj mishra <yashraj06112001@gmail.com>
2025-04-10 18:17:44 +0530

    added comments to understand

--- routes/api.php
+++ routes/api.php
@@ -24,19 +24,26 @@ use App\Http\Controllers\productListingController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// API to signUp the user
 Route::post("/signUp",[signUp::class,"signUpHandler"])->name("signUp");
+//API to logged in the user
 Route::post("/Login",[LoginHandler::class,'loginHandling'])->name("Login");
+//API to verify user logged in or not
 Route::middleware('auth:sanctum')->post("/Verify",[VerifyPageAcessController::class,"verify"])->name("Verify");
 
 Route::post('/cart-products', [CartController::class, 'getCartProducts']);
 Route::post('/cart-remove', [CartController::class, 'removeFromCart']);
 Route::post('/cart/update-check', [CartController::class, 'updateCartCheckStatus']);
+//API to get all product-category and sub-category
 Route::get("/product-category",[ProductCategoryController::class,"categorySubcategary"])->name("category");
+//API to get price-category range
 Route::get("/price-category",[ProductCategoryController::class,"PriceCategory"])->name("price-category");
+// logout api's to log out from system
 Route::middleware('auth:sanctum')->post("/logout",[logoutHandlerController::class,"logout"])->name("logout");
 Route::middleware('auth:sanctum')->post("/logoutAll",[VerifyPageAcessController::class,"logoutAll"])->name("logoutAll");
+//API to get all the products List
 Route::get("/productListing",[productListingController::class,"ShowProducts"])->name("productShow");
-// api to give subcategories based on category
+// api to give subcategories based on category -> send {category} in request to get all types of subcategories related to that category}
 Route::post("/catSubCat",[ProductCategoryController::class,"categoryBasedSubcategory"])->name("subCategory");
 // Product after applying filters of category, subcategory, maxPrice, minPrice
 Route::post('/productFilteredList',[productListingController::class,'productAfterFilterListing'])->name("filteredProduct");
