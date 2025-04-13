5ce73f36b4d02a377cbf6defaa5cb6f6c9715898
yash raj mishra <yashraj06112001@gmail.com>
2025-04-13 23:42:33 +0530

    added inactive option rather than inActive

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -32,7 +32,7 @@ class ProductCategoryController extends Controller
 
     public function PriceCategory(Request $request)
     {   $minPrice = DB::table('products')
-        ->where('status', '!=', 'inActive') // Exclude inactive products
+        ->where('status', '!=', 'inactive') // Exclude inactive products
         ->whereNotNull('discount') // Ensure discount is not null
         ->where('discount', '!=', '') // Ensure discount is not empty
         ->selectRaw('MIN(CAST(discount AS SIGNED)) as min_discount') // Convert VARCHAR to integer
@@ -40,7 +40,7 @@ class ProductCategoryController extends Controller
         // If you want to return 0 when no discounts exist instead of null:
        $minPrice = $minPrice ?? 0;
        $maxPrice = DB::table('products')
-    ->where('status', '!=', 'inActive') // Skip inactive products
+    ->where('status', '!=', 'inactive') // Skip inactive products
     ->whereNotNull('discount') // Ensure discount is not NULL
     ->where('discount', '!=', '') // Skip empty strings
     ->selectRaw('MAX(CAST(discount AS SIGNED)) as max_discount') // Convert VARCHAR → INT & get MAX
