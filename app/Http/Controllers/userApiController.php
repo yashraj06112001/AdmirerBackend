7c7ff5e7341594f21c231383eaccfd0933472ef6
yash raj mishra <yashraj06112001@gmail.com>
2025-04-24 10:50:57 +0530

    added phone number also to come from api named userApiController getAddress

--- app/Http/Controllers/userApiController.php
+++ app/Http/Controllers/userApiController.php
@@ -15,7 +15,7 @@ class userApiController extends Controller
     ->leftJoin('shiping_address as sa','od.order_id','=','sa.order_id')
     ->leftJoin('state_list','state_list.id','=','sa.state')
     ->leftJoin('countries','countries.id','=','sa.country')
-     ->select('sa.first_name','sa.last_name','sa.flat','sa.street','sa.city','sa.locality','state_list.state','countries.country_name','sa.zip_code','sa.addr_type')
+     ->select('sa.first_name','sa.last_name','sa.flat','sa.street','sa.city','sa.locality','state_list.state','countries.country_name','sa.zip_code','sa.addr_type','sa.phone')
      ->where('sa.order_id','=',$order_id)
      ->first();
   
